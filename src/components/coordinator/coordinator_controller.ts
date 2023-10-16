eafe5b8b9deeaed46a7b525e74858c89c5e71209
ZRunner <z.runner.mc@gmail.com>
2023-10-16 11:50:38 -0400

    refact(crd): GET returns list of ID instead of objects

--- src/components/coordinator/coordinator_controller.ts
+++ src/components/coordinator/coordinator_controller.ts
@@ -1,4 +1,3 @@
-import { CoordinatorNode } from "@prisma/client";
 import { Request, Response } from "express";
 
 import Database from "../../database";
@@ -9,12 +8,12 @@ export async function getNodes(req: Request, res: Response) {
     // create a map of CoordinatorId => list of Nodes
     const nodes = await db.coordinatorNode.findMany();
     const coordinatorIds = (await db.coordinator.findMany({ select: { id: true } })).map((c) => c.id);
-    const coordinatorNodes: Record<number, CoordinatorNode[]> = coordinatorIds.reduce((acc, id) => {
+    const coordinatorNodes: Record<number, number[]> = coordinatorIds.reduce((acc, id) => {
         acc[id] = [];
         return acc;
-    }, {} as Record<number, CoordinatorNode[]>);
+    }, {} as Record<number, number[]>);
     nodes.forEach((node) => {
-        coordinatorNodes[node.coordinatorId].push(node);
+        coordinatorNodes[node.coordinatorId].push(node.id);
     });
 
     return res.status(200).send({ success: true, data: coordinatorNodes });
