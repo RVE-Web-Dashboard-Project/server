7b94807b2be811cc56c7eab8a26bcf3a25678b24
ZRunner <z.runner.mc@gmail.com>
2023-10-16 11:48:02 -0400

    feat(crd): add authentication to GET endpoint

--- src/components/coordinator/coordinator_router.ts
+++ src/components/coordinator/coordinator_router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
+import { isAuthenticatedCheck } from "../../auth/middlewares";
 import * as CRD from "./coordinator_controller";
 
 const router = express.Router();
 
-router.get("/nodes", CRD.getNodes);
+router.get("/nodes", isAuthenticatedCheck, CRD.getNodes);
 
 export default router;
