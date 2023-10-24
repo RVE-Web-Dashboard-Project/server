f7eb62e06d3a025a7f77d5136811d1f99c714a9b
ZRunner <z.runner.mc@gmail.com>
2023-10-24 19:13:49 -0400

    feat(env): add MQTT env variables

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,13 +1,24 @@
 import type { User as PrismaUser } from "@prisma/client";
 
+interface ProcessEnvVariables {
+  PORT?: string;
+  DATABASE_URL: string;
+  JWT_SECRET: string;
+  JWT_TOKEN_EXPIRATION_DAYS?: string;
+  CORS_ACCEPTED_DOMAINS: string;
+  MQTT_BROKER_URL: string;
+  MQTT_BROKER_PORT: string;
+  MQTT_SENDCMD_TOPIC: string;
+  MQTT_RECEIVE_TOPIC: string;
+  MQTT_USERNAME: string;
+  MQTT_USER_ID: string;
+  MQTT_PASSWORD: string;
+}
+
 declare global {
   namespace NodeJS {
-    interface ProcessEnv {
-      DATABASE_URL: string;
-      JWT_SECRET: string;
-      JWT_TOKEN_EXPIRATION_DAYS?: string;
-      CORS_ACCEPTED_DOMAINS: string;
-    }
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface ProcessEnv extends ProcessEnvVariables {}
   }
 
   namespace Express {
