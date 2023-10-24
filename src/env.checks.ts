a5395bca4fd8d34a4fe39fc10f20d7f4e194b1c4
ZRunner <z.runner.mc@gmail.com>
2023-10-24 19:13:59 -0400

    feat(env): add env variables checking

---
+++ src/env.checks.ts
@@ -0,0 +1,46 @@
+import { ProcessEnvVariables } from "./types";
+
+type EnvVariableNames = (keyof ProcessEnvVariables)[];
+
+const requiredEnvVariables: EnvVariableNames = [
+    "DATABASE_URL",
+    "JWT_SECRET",
+    "CORS_ACCEPTED_DOMAINS",
+    "MQTT_BROKER_URL",
+    "MQTT_BROKER_PORT",
+    "MQTT_SENDCMD_TOPIC",
+    "MQTT_RECEIVE_TOPIC",
+    "MQTT_USERNAME",
+    "MQTT_USER_ID",
+    "MQTT_PASSWORD",
+];
+
+
+/**
+ * Checks if all required environment variables are set.
+ * @returns true if all required environment variables are set, false otherwise.
+ */
+export function checkEnvironmentVariables() {
+    const missingVariables = requiredEnvVariables.filter(key => process.env[key] === undefined);
+    if (missingVariables.length > 0) {
+        console.error("FATAL ERROR: Missing environment variables:", missingVariables);
+        return false;
+    }
+
+    if (process.env.PORT && isNaN(Number(process.env.PORT))) {
+        console.error("FATAL ERROR: PORT is not a number");
+        return false;
+    }
+
+    if (process.env.JWT_SECRET.length < 15) {
+        console.error("FATAL ERROR: JWT_SECRET is too short");
+        return false;
+    }
+
+    if (isNaN(Number(process.env.MQTT_BROKER_PORT))) {
+        console.error("FATAL ERROR: MQTT_BROKER_PORT is not a number");
+        return false;
+    }
+
+    return true;
+}
