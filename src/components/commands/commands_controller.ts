879007d23f995a424d64d3854b3c7f24f9cc71f8
ZRunner <z.runner.mc@gmail.com>
2023-11-13 14:15:11 -0500

    refact(core): remove "success" JSON answer

--- src/components/commands/commands_controller.ts
+++ src/components/commands/commands_controller.ts
@@ -9,28 +9,25 @@ const db = Database.getInstance();
 const mqttClient = MQTTClient.getInstance();
 
 export async function getBrokerConnectionStatus(req: Request, res: Response) {
-    return res.status(200).send({ success: true, data: mqttClient.connectionStatus });
+    return res.status(200).send({ status: mqttClient.connectionStatus });
 }
 
 export async function getCommandsList(req: Request, res: Response) {
-    return res.status(200).send({
-        success: true,
-        data: COMMANDS_LIST,
-    });
+    return res.status(200).send(COMMANDS_LIST);
 }
 
 export async function sendCommand(req: Request<unknown, unknown, SendCommandParams>, res: Response) {
      // check arguments existense
      if (!is<SendCommandParams>(req.body)) {
         res._err = "Missing or invalid arguments";
-        return res.status(400).send({ success: false, message: res._err });
+        return res.status(400).send(res._err);
     }
 
     // check command ID existence
     const command = COMMANDS_LIST.find((c) => c.id === req.body.commandId);
     if (!command) {
         res._err = "Command not found";
-        return res.status(404).send({ success: false, error: res._err });
+        return res.status(404).send(res._err);
     }
 
     // check nodes parameter
@@ -39,7 +36,7 @@ export async function sendCommand(req: Request<unknown, unknown, SendCommandPara
         // check node IDs existence
         if (req.body.nodeIds === undefined || req.body.nodeIds.length === 0) {
             res._err = "Node ID is required";
-            return res.status(400).send({ success: false, error: res._err });
+            return res.status(400).send(res._err);
         }
 
         // check if each node ID is present in at least one coordinator
@@ -56,7 +53,7 @@ export async function sendCommand(req: Request<unknown, unknown, SendCommandPara
             const correspondingCoordinatorId = coordinatorNodes.filter((cn) => cn.id === nodeId).map((cn) => cn.coordinatorId)[0];
             if (!correspondingCoordinatorId) {
                 res._err = `Node ${nodeId} not found`;
-                return res.status(404).send({ success: false, error: res._err });
+                return res.status(404).send(res._err);
             }
             coordinatorToNodesMap.set(
                 correspondingCoordinatorId, [...(coordinatorToNodesMap.get(correspondingCoordinatorId) ?? []), nodeId]
@@ -67,7 +64,7 @@ export async function sendCommand(req: Request<unknown, unknown, SendCommandPara
     // check parameters count
     if (command.parameters.length !== (req.body.parameters?.length ?? 0)) {
         res._err = "Parameters count mismatch";
-        return res.status(400).send({ success: false, error: res._err });
+        return res.status(400).send(res._err);
     }
 
     // check parameters values
@@ -77,11 +74,11 @@ export async function sendCommand(req: Request<unknown, unknown, SendCommandPara
         const value = parameters[i];
         if ((param.minValue && value < param.minValue) || (param.maxValue && value > param.maxValue)) {
             res._err = `Parameter ${param.name} value out of range`;
-            return res.status(400).send({ success: false, error: res._err });
+            return res.status(400).send(res._err);
         }
         if (param.type === "int" && !Number.isInteger(value)) {
             res._err = `Parameter ${param.name} value is not an integer`;
-            return res.status(400).send({ success: false, error: res._err });
+            return res.status(400).send(res._err);
         }
     }
 
@@ -91,7 +88,7 @@ export async function sendCommand(req: Request<unknown, unknown, SendCommandPara
     // check MQTT broker connection
     if (!mqttClient.connected) {
         res._err = "MQTT broker not connected";
-        return res.status(500).send({ success: false, error: res._err });
+        return res.status(500).send(res._err);
     }
 
     // send command
@@ -109,8 +106,8 @@ export async function sendCommand(req: Request<unknown, unknown, SendCommandPara
     } catch (err) {
         console.error("Failed to send command:", err);
         res._err = "Failed to send command";
-        return res.status(500).send({ success: false, error: res._err });
+        return res.status(500).send(res._err);
     }
 
-    return res.status(200).send({ success: true });
+    return res.sendStatus(200);
 }
