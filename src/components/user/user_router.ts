68abed345e220323689a903c0590b7d494952488
ZRunner <z.runner.mc@gmail.com>
2023-11-13 14:35:43 -0500

    feat(invite): add GET invitation info endpoint

--- src/components/user/user_router.ts
+++ src/components/user/user_router.ts
@@ -6,6 +6,7 @@ import * as USR from "./user_controller";
 const router = express.Router();
 
 router.get("/me", isAuthenticatedCheck, USR.getMe);
+router.get("/invitation/:code", USR.getInvitationInfo);
 
 router.post("/login", USR.login);
 router.post("/logout", isAuthenticatedCheck, USR.logout);
