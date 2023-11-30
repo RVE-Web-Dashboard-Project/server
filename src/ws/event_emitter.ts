25f4978c3dec5b556530691763fe4e94f040b7d7
ZRunner <z.runner.mc@gmail.com>
2023-11-30 11:45:37 -0500

    feat(event): add MQTT response event

--- src/ws/event_emitter.ts
+++ src/ws/event_emitter.ts
@@ -1,12 +1,14 @@
 import { EventEmitter } from "stream";
 
-import { MQTTConnectionStatus } from "../mqtt/mqtt_types";
+import { MQTTConnectionStatus, MQTTReceivedMessage } from "../mqtt/mqtt_types";
 
 interface MyEventEmitter {
     emit(event: "mqtt_connection_update", status: MQTTConnectionStatus): boolean;
+    emit(event: "mqtt_response_received", data: MQTTReceivedMessage): boolean;
     emit(event: "test_body", json: object): boolean;
 
     on(event: "mqtt_connection_update", listener: (status: MQTTConnectionStatus) => void): this;
+    on(event: "mqtt_response_received", listener: (data: MQTTReceivedMessage) => void): this;
     on(event: "test_body", listener: (json: object) => void): this;
 }
 
