aa7bfb8c05dae2ac22131f59bf610aba3c9eb88b
ZRunner <z.runner.mc@gmail.com>
2023-12-06 12:06:22 -0500

    feat(user): add list of users endpoint

--- src/components/user/user_router.ts
+++ src/components/user/user_router.ts
@@ -5,12 +5,18 @@ import * as USR from "./user_controller";
 
 const router = express.Router();
 
+// ----- USER -----
+
 router.get("/me", isAuthenticatedCheck, USR.getMe);
-router.get("/invitation/:code", USR.getInvitationInfo);
+router.get("/list", isAdminCheck, USR.listUsers);
 
 router.post("/login", USR.login);
 router.post("/logout", isAuthenticatedCheck, USR.logout);
 router.post("/edit-password", isAuthenticatedCheck, USR.changePassword);
+
+// ----- INVITATION -----
+
+router.get("/invitation/:code", USR.getInvitationInfo);
 
 router.post("/invite", isAdminCheck, USR.inviteUser);
 router.post("/invitation/:code", USR.acceptInvitation);
