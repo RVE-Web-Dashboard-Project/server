57acf2e98eaf89a96f7468438f5b90cf447506d1
ZRunner <z.runner.mc@gmail.com>
2023-12-21 19:07:55 +0100

    feat(nodes): add endpoint to edit nodes list

--- src/components/coordinator/coordinator_router.ts
+++ src/components/coordinator/coordinator_router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { isAuthenticatedCheck } from "../../auth/middlewares";
+import { isAdminCheck, isAuthenticatedCheck } from "../../auth/middlewares";
 import * as CRD from "./coordinator_controller";
 
 const router = express.Router();
 
 router.get("/nodes", isAuthenticatedCheck, CRD.getNodes);
+
+router.put("/nodes", isAdminCheck, CRD.editNodes);
 
 export default router;
